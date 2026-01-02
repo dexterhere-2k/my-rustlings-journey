cb9dcbbeda27516d2159f25f0c4c972e6a3168b7
dexterhere-2k <dmirchandani8@gmail.com>
2026-01-02 23:42:16 +0530

    completed few exercises

---
+++ exercises/01_variables/variables3.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32=10; //Initialise x with a value.
+
+    println!("Number {x}");
+}
---
+++ exercises/01_variables/variables4.rs
@@ -0,0 +1,8 @@
+// TODO: Fix the compiler error.
+fn main() {
+    let mut x = 3; // Declare x as a mut variable.
+    println!("Number {x}");
+
+    x = 5; // Don't change this line
+    println!("Number {x}");
+}
