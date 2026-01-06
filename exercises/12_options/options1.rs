551f1b5c43566be15cae5cb6b488aa1189731029
dexterhere-2k <deepakmirchandani.ai28@jecrc.ac.in>
2026-01-06 21:00:27 +0530

    Completed 12_options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,11 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    match hour_of_day {
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        _ => None,
+    }
 }
 
 fn main() {
@@ -19,7 +24,7 @@ mod tests {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
         let ice_creams = maybe_ice_cream(12);
-
+        ice_creams.unwrap();
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
 
